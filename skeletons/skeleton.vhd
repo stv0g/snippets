94cd7efbed934cb98cbd61a5f72ccc59f9188d6f
Steffen Vogel <post@steffenvogel.de>
2020-12-31 12:36:37 +0100

    update skeletons

--- skeletons/skeleton.vhd
+++ skeletons/skeleton.vhd
@@ -2,30 +2,14 @@
 --
 -- Long description
 --
--- @copyright	2016 Steffen Vogel
--- @license	http://www.gnu.org/licenses/gpl.txt GNU Public License
--- @author	Steffen Vogel <post@steffenvogel.de>
--- @link	http://www.steffenvogel.de
+-- @copyright 2021, Steffen Vogel
+-- @license   http://www.gnu.org/licenses/gpl.txt GNU Public License
+-- @author    Steffen Vogel <post@steffenvogel.de>
+-- @link      https://www.steffenvogel.de
 -- @package
 -- @category
 -- @since
 --
---------------------------------------------------------------------------------
---
--- This file is part of [...]
---
--- [...] is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- any later version.
---
--- [...] is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with [...]. If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------
 
 library ieee;
